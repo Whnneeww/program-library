f0129e82c4b5a20928ca6a169d5bdb5101d49f3d
The_Finite <143406753+Whnneeww@users.noreply.github.com>
2024-09-07 11:40:57 +0900

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,5 @@
 async function JslibraryInstall(url) {
 document.getElementById('addLibrary').addEventListener('click', function() {
-            // jQueryライブラリを動的に追加
             var script = document.createElement('script');
             script.src = url;
             script.onload = function() {
